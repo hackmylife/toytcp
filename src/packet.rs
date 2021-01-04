96210459fe7e25b62c06bde2fcaf3841e09659e6
nw <nw@linecorp.com>
2021-01-04 23:53:35 +0900

    add tcp header getter /setter fanctions

--- src/packet.rs
+++ src/packet.rs
@@ -10,6 +10,29 @@ const TCP_HEADER_SIZE: usize = 20;
 pub struct TCPPacket {
     buffer: Vec<u8>,
 }
+/*
+   see http://www5d.biglobe.ne.jp/stssk/rfc/rfc793j.html
+   TCP Header Format
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |          Source Port          |       Destination Port        |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                        Sequence Number                        |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                    Acknowledgment Number                      |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |  Data |           |U|A|P|R|S|F|                               |
+   | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
+   |       |           |G|K|H|T|N|N|                               |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |           Checksum            |         Urgent Pointer        |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                    Options                    |    Padding    |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                             data                              |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+*/
 
 impl TCPPacket{
     pub fn new(payload_len: usize) -> Self {
@@ -18,6 +41,46 @@ impl TCPPacket{
         }
     }
 
+    // getter
+    pub fn get_src(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[0], self.buffer[1]])
+    }
+
+    pub fn get_dest(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[2], self.buffer[3]])
+    }
+
+    pub fn get_seq(&self) -> u32 {
+        u32::from_be_bytes([
+            self.buffer[4],
+            self.buffer[5],
+            self.buffer[6],
+            self.buffer[7],
+        ])
+    }
+
+    pub fn get_ack(&self) -> u32 {
+        u32::from_be_bytes([
+            self.buffer[8],
+            self.buffer[9],
+            self.buffer[10],
+            self.buffer[11],
+        ])
+    }
+
+    pub fn get_flag(&self) -> u8 {
+        u8::from_be_bytes([self.buffer[13]])
+    }
+
+    pub fn get_window_size(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[14], self.buffer[15]])
+    }
+
+    pub fn get_checksum(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[16], self.buffer[17]])
+    }
+
+    // setter
     pub fn set_src(&mut self, port: u16) {
         self.buffer[0..2].copy_from_slice(&port.to_be_bytes())
     }
@@ -26,8 +89,37 @@ impl TCPPacket{
         self.buffer[2..4].copy_from_slice(&port.to_be_bytes())
     }
 
+    pub fn set_seq(&mut self, num: u32) {
+        self.buffer[4..8].copy_from_slice(&num.to_be_bytes())
+    }
+
+    pub fn set_ack(&mut self, num: u32) {
+        self.buffer[8..12].copy_from_slice(&num.to_be_bytes())
+    }
+
     pub fn set_flag(&mut self, flag: u8) {
         self.buffer[13] = flag;
+    }
+
+    pub fn set_window_size(&mut self, window: u16) {
+        self.buffer[14..16].copy_from_slice(&window.to_be_bytes())
+    }
+
+    pub fn set_checksum(&mut self, payload: &[u8]) {
+        self.buffer[TCP_HEADER_SIZE..TCP_HEADER_SIZE + payload.len() as usize]
+            .copy_from_slice(payload)
+    }
+
+    pub fn is_correct_checksum(&self, local_addr: Ipv4Addr, remote_addr: Ipv4Addr) -> bool {
+        self.get_checksum()
+            == util::ipv4_checksum(
+            &self.packet(),
+            8,
+            &[],
+            &local_addr,
+            &remote_addr,
+            IpNextHeaderProtocols::Tcp,
+        )
     }
 }
 
@@ -40,3 +132,4 @@ impl Packet for TCPPacket {
         &self.buffer[TCP_HEADER_SIZE..]
     }
 }
+
